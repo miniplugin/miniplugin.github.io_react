00e052dc5efb6fc0f3f84b2f317daa9e3a2f6511
kim ilguk <boramcom@daum.net>
2020-03-27 18:45:19 +0900

    빌드 및 배포.

---
+++ app_covid19/build/precache-manifest.b47822a45bde72ab29c1b47862894de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5eb5364019d889671ae361a1ba54358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fac3666f78f5d57a58e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6a25523ee68792104f80",
+    "url": "/static/js/2.34a3bfdb.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.34a3bfdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fac3666f78f5d57a58e",
+    "url": "/static/js/main.a27ac352.chunk.js"
+  },
+  {
+    "revision": "5096569a31c408a96d0e",
+    "url": "/static/js/runtime-main.140da53e.js"
+  }
+]);
